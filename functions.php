eeecf0d938bbdfee822c64544045fab8f1edffa8
Edward Caissie <edward.caissie@gmail.com>
2016-11-27 16:14:44 -0500

    - Revert changes that slipped through with the "Create Pull Request" tests

--- functions.php
+++ functions.php
@@ -229,14 +229,3 @@ function jimini_no_active_widgets() {
 }
 
 add_action( 'wp_enqueue_scripts', 'jimini_no_active_widgets' );
-
-/**
- * Nada
- *
- * A meaningless function to accomplish nothing.
- *
- * @return null
- */
-function jimini_nada() {
-	return null;
-}
